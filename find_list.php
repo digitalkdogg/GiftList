f6086adf96c39773e32b83b45b85792a2f7dce7c
Kevin <digitalkdogg@gmail.com>
2013-05-20 19:06:12 -0400

    Rewrote search to be more effiencient

--- find_list.php
+++ find_list.php
@@ -1,52 +1,32 @@
 <?php
-var_dump('hi');
 $response = array();
 $name = $_POST['name'];
 
 list($fname, $lname) = split(' ', $name,2);
 
-$con = mysql_connect("localhost","Kevin","I not telling");
+$con = mysql_connect("localhost","somedude","somepassword");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
 
-mysql_select_db("GiftList", $con);
+mysql_select_db("somedatabase", $con);
 $return_arr = array();
 
-$fetch = mysql_query("select * from owner where first_name = '" . $name . "'"); 
-$num_rows = mysql_num_rows($fetch);
-if ($num_rows > 0) :
-	$found = true;
-else:
-	$fetch = mysql_query("select * from owner where last_name = '" . $name . "'"); 
-	$num_rows = mysql_num_rows($fetch);
-	if ($num_rows > 0) :
-		$found = true;
-	else:
-		$fetch = mysql_query("select * from owner where first_name = '" . $fname . "' and last_name = '" . $lname . "'"); 
-		$num_rows = mysql_num_rows($fetch);
-		if ($num_rows > 0) :
-			$found =true;
-		else:
-			$return_arr = array('found' => 'no');
-		endif;
-	endif;	
-endif;
-
-
-if ($found ==true):
+$fetch = mysql_query("select * from owner where first_name = '" . $name . "' or last_name = '" .$name . "'");
+if (mysql_num_rows($fetch) > 0) {
 	$return_arr = array('found' => 'yes');
 
-while ($row = mysql_fetch_array($fetch, MYSQL_ASSOC)) {
-    $row_array['id'] = $row['owner_id'];
-    $row_array['first_name'] = $row['first_name'];
-    $row_array['last_name'] = $row['last_name'];
-	$row_array['html'] = "<a href = 'list.php/gift/" . $row['first_name'] . "'>" . $row['first_name'] . " " . $row['last_name'] . "</a>";
+ 	while ($row = mysql_fetch_array($fetch, MYSQL_ASSOC)) {
+    	 $row_array['id'] = $row['owner_id'];
+     	$row_array['first_name'] = $row['first_name'];
+     	$row_array['last_name'] = $row['last_name'];
+ 		$row_array['html'] = "<a href = 'list.php/gift/" . $row['first_name'] . "'>" . $row['first_name'] . " " . $row['last_name'] . "</a>";
 
-    array_push($return_arr,$row_array);
+    	array_push($return_arr,$row_array);
+ 	}
 }
-endif;
+
 echo json_encode($return_arr);
 
 mysql_close($con);
