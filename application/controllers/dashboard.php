7c478801fd5370a195d32917cda4ab4d26965d19
Kevin <kwb111@psu.edu>
2013-08-23 16:23:38 -0400

    fixed spacing issues with dashboard controller

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -9,121 +9,70 @@ class Dashboard extends CI_Controller {
     {
 
        	parent::__construct();
-
        	$this->load->model('db_model');	
-
 		$this->load->model('html_model');
-
 		$this->load->model('gift_model');
-
 		$this->load->library('session');
-
     }
 
 
 
 	public function index ($name = null) 
-
 	{
 		if ($name==null) {
 			 redirect('/', 'refresh');
 		}
-		
 		$session_data = ($this->session->all_userdata());
-	
 		$owner = $this->db_model->get_owner($name);
-
-		
-
-		    $content = $this->load->view('html_begin', '', true);
-
-			$content .= $this->load->view('css_jquery_files', array('dashboard'=>true), true);
-
-		 	$content .= $this->load->view('body_header', array('first_name'=>$owner['first_name'],
-									 'last_name'=>$owner['last_name'],
-									 'user_name'=> $owner['user_name'],
-									  'header_title'=>'Dashboard'), true);
+		$content = $this->load->view('html_begin', '', true);
+		$content .= $this->load->view('css_jquery_files', array('dashboard'=>true), true);
+		$content .= $this->load->view('body_header', array('first_name'=>$owner['first_name'],
+			'last_name'=>$owner['last_name'],
+			'user_name'=> $owner['user_name'],
+			'header_title'=>'Dashboard'), true);
 	
-
-		 	$content .= "<div id = 'content'>";
-
-			if (isset($session_data['login'])):
-
+	 	$content .= "<div id = 'content'>";
+		if (isset($session_data['login'])):
 		 	$list_items = $this->db_model->get_list_for_owner($owner['owner_id']);
-
 		 	if ($list_items) :
-
 		 		$content .= "<div id = 'gifts' class = 'items'>";
-
 		 		$content .= "<div class = 'dash_title'>Lists<button class='gift' data-id = ". $owner['owner_id'] .">New Gift List</button></div>";
-
 		 		foreach ($list_items as $list) :
-
 		 			$items = $this->db_model->get_giftitems_list($list->list_id);
-
 		 			$content .= $this->load->view('dashboard/dashboard_gift_start', array('title' => $list->title, 'list_id'=>$list->list_id), true);
-
 		 			foreach ($items as $item) :
-
 		 				$content .= $this->load->view('dashboard/dashboard_gift_dets', array('items' => $item), true);
-
 		 			endforeach;
-
 		 			$content .="</div><!--end item --></div><!--end item wrapper> -->";
-
 		 		endforeach;
-
 		 		$content .= "</div><!--end class items -->";
-
 		 	endif;
-
-		 
 
 		 	if ($owner):
-
 		 		$content .= "<div id = 'owner' class = 'items'>";
-
 		 		$content .= "<div class = 'dash_title'>Owner Info</div>";
-
 		 		$content .= $this->load->view('dashboard/dashboard_gift_start', array('title' => $owner['user_name']), true);
-
 		 		$content .= $this->load->view('dashboard/dashboard_owner_dets', array('owner'=>$owner), true);
-
 		 		$content .="</div><!--end item --></div><!--end item wrapper> -->";
-
 		 		$content .= "</div><!--end class items -->";	
-
 		 	endif;
 
-
-
 		 	$admin = $this->db_model->get_giftlist_admin($owner['owner_id']);
-
 			if($admin) :
-
 				$content .= "<div id = 'admin' class = 'items'>";
-
 		 		$content .= "<div class = 'dash_title'>Gift List Admin</div>";
-
 		 		$content .= $this->load->view('dashboard/dashboard_gift_start', array('title' => $admin->first_name.' '. $admin->last_name), true);
-
 		 		$content .= $this->load->view('dashboard/dashboard_admin_dets', array('admin'=>$admin), true);
-
 		 		$content .="</div><!--end item --></div><!--end item wrapper> -->";
-
 		 		$content .= "</div><!--end class items -->";	
-
 		 	endif; 
 
-			else:
-				$content .= $this->load->view('dashboard/login_form', '', true);
-			endif;
-
-		 	$content .= $this->html_model->load_html_close('true');
-
-		    $html = array ('html' => $content);
-
-			$this->load->view('print_html', $html);
+		else:
+			$content .= $this->load->view('dashboard/login_form', '', true);
+		endif;
+		$content .= $this->html_model->load_html_close('true');
+		$html = array ('html' => $content);
+		$this->load->view('print_html', $html);
 
 			//echo $content;
 //$this->session->unset_userdata('login');
@@ -140,7 +89,6 @@ class Dashboard extends CI_Controller {
 	                   'login'  => 'true',
 	                   'login_user' => $_POST['user_name']
 	               );
-
 				$this->session->set_userdata($newdata);
 		    	echo json_encode(array('login' => 'true', 'message'=>'Login Success....Redirecting!'));
 			} else {
@@ -150,7 +98,6 @@ class Dashboard extends CI_Controller {
 		} else {
 			echo json_encode(array('message'=>'User does not exsist'));
 		}
-
 	}
 
 
@@ -319,8 +266,5 @@ class Dashboard extends CI_Controller {
 			redirect(site_url() . '/dashboard/' . $owner->user_name , 'refresh');
 		 endif;
 	}
-
-
-
 
  }//end dashboard class
